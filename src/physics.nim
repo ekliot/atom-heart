0309623dcd2c6c891b8ec60c7e1bde2b4970ae13
ekliot <ekliot@gmail.com>
2018-12-02 17:24:45 -0500

    update physics to be a static lib, rather than a singleton Node

--- src/physics.nim
+++ src/physics.nim
@@ -5,48 +5,47 @@ A collection of physics constants and helper methods
 import
   godot, node
 
-const FrictionAir*: float = 0.3
-const Gravity*: float = 25.0
-const Up*: Vector2 = vec2(0, -1)
+const FRICTION_AIR*: float = 0.3
+const GRAVITY*: float = 25.0
+const UP*: Vector2 = vec2(0, -1)
 
-gdobj Physics of Node:
-  proc apply_friction_to_flt*(flt, friction: float, to = 0.0): float {.gdExport.} =
-    ## apply a friction coefficient to a float until a target value
+proc apply_friction_to_flt*(flt, friction: float, to = 0.0): float =
+  ## apply a friction coefficient to a float until a target value
 
-    # result = lerp(flt, friction, to)
-    result = (1 - friction) * flt + friction * to
-      # NIMIFY GDScript.lerp for floats not available in godot-nim?
-      # equation from https://en.wikipedia.org/wiki/Linear_interpolation#Programming_language_support
+  (1 - friction) * flt + friction * to
+    # lerp(flt, friction, to)
+    # NIMIFY GDScript.lerp for floats not available in godot-nim?
+    # equation from https://en.wikipedia.org/wiki/Linear_interpolation#Programming_language_support
 
-  proc apply_friction_to_vec*(vec: Vector2, friction:float, to: Vector2 = vec2()): Vector2 {.gdExport.} =
-    ## apply a friction vector to a vector until a target value
+proc apply_friction_to_vec*(vec: Vector2, friction:float, to: Vector2 = vec2()): Vector2 =
+  ## apply a friction vector to a vector until a target value
 
-    result = vec2(
-      apply_friction_to_flt(vec.x, friction, to.x),
-      apply_friction_to_flt(vec.y, friction, to.y)
-    )
+  vec2(
+    apply_friction_to_flt(vec.x, friction, to.x),
+    apply_friction_to_flt(vec.y, friction, to.y)
+  )
 
-  proc cap_velocity*(vel, cap: Vector2): Vector2 {.gdExport.} =
-    ## clamp a velocity vector between upper and lower bounds
-    ## cap axes must be >=0, or else that axis will not be clamped
-    ## TODO built in friction?
+proc cap_velocity*(vel, cap: Vector2): Vector2 =
+  ## clamp a velocity vector between upper and lower bounds
+  ## cap axes must be >=0, or else that axis will not be clamped
+  ## TODO built in friction?
 
-    #[
-    if friction:
-      if abs(vel.x) > abs(cap.x):
-        var x_dir = sign(vel.x)
-        vel.x = apply_friction_to_flt(vel.x, friction, x_dir * cap.x)
+  #[
+  if friction:
+    if abs(vel.x) > abs(cap.x):
+      var x_dir = sign(vel.x)
+      vel.x = apply_friction_to_flt(vel.x, friction, x_dir * cap.x)
 
-        if abs(vel.y) > abs(cap.y):
-          var y_dir = sign(vel.y)
-          vel.y = apply_friction_to_flt(vel.y, friction, y_dir * cap.y)
-        else:
-    ]#
+      if abs(vel.y) > abs(cap.y):
+        var y_dir = sign(vel.y)
+        vel.y = apply_friction_to_flt(vel.y, friction, y_dir * cap.y)
+      else:
+  ]#
 
-    let cap_x = if cap.x < 0: vel.x else: cap.x
-    let cap_y = if cap.y < 0: vel.y else: cap.y
+  let cap_x = if cap.x < 0: vel.x else: cap.x
+  let cap_y = if cap.y < 0: vel.y else: cap.y
 
-    result = vec2(
-      min(max(vel.x, -cap_x), cap_x),
-      min(max(vel.y, -cap_y), cap_y)
-    )
+  result = vec2(
+    min(max(vel.x, -cap_x), cap_x),
+    min(max(vel.y, -cap_y), cap_y)
+  )
