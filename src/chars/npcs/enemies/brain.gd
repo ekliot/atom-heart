6dfadd190c7119c867f1986073142df7a46f4a03
ekliot <ekliot@gmail.com>
2019-01-01 16:07:49 -0500

    filling out the brain

--- src/chars/npcs/enemies/brain.gd
+++ src/chars/npcs/enemies/brain.gd
@@ -6,17 +6,30 @@ Controller class for enemy AI. To be overriden by individual enemies
 
 extends Node
 
-onready var body = get_parent()
-onready var states = body.FSM.get_children()
+onready var BODY = get_parent()
+onready var FSM = BODY.FSM
+onready var STATES = FSM
+
+var interruptor = null
+
+func _ready():
+  # body.FSM.connect('state_change', self, '_on_state_change')
+  # connect to states?
+  pass
 
 func _on_state_change(state_from, state_to):
+  """
+  to be overridden by other brains
+  connected to body.FSM in enemy.gd
+  """
   pass
+
+"""
+=== PERCEPTION
+"""
 
 func look_at(dir):
   look_dir = dir
-
-func go_to(pos):
-  pass
 
 func line_of_sight():
   """
@@ -30,8 +43,32 @@ func can_see(obj):
   """
   return obj in line_of_sight()
 
+
+"""
+=== STATE DIRECTIVES
+"""
+
 func idle():
-  """
-  directive on what to do while idling
-  """
-  pass
+  if interruptor:
+    return interruptor
+  return null
+
+func airborne():
+  if interruptor:
+    return interruptor
+  return null
+
+func move(to):
+  if interruptor:
+    return interruptor
+  return null
+
+func chase(target):
+  if interruptor:
+    return interruptor
+  return null
+
+func attack(target):
+  if interruptor:
+    return interruptor
+  return null
